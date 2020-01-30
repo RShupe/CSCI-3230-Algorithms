8cb04af8c4f2b4b9f815856310caaf3fa8fb5e7e
Ryan <ryanthecat17@gmail.com>
2020-01-30 18:45:54 -0500

    Lab and some project work

---
+++ GrowthofFunctions/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Diagnostics;
+using System.Numerics;
+
+namespace Lab_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            while (1 == 1)
+            {
+                Stopwatch sw = new Stopwatch();
+                Console.WriteLine("Enter a number: ");
+                ulong input = Convert.ToUInt64(Console.ReadLine());
+
+
+                sw.Start();
+                nFactorial(input);
+                sw.Stop();
+
+                Console.WriteLine("n = " + input);
+                Console.WriteLine("Time used: {0} seconds.", sw.Elapsed.TotalMilliseconds / 1000);
+            }
+        }
+
+        private static void nsquaredgrowth(int n)
+        {
+            int sum = 0;
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    sum = sum + 1;
+                }
+            }
+        }
+
+        private static void ncubedgrowth(int n)
+        {
+            int sum = 0;
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    for (int k = 0; k < n; k++)
+                    {
+                        sum = sum + 1;
+                    }
+                }
+            }
+        }
+
+        private static void nquadgrowth(int n)
+        {
+            int sum = 0;
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    for (int k = 0; k < n; k++)
+                    {
+                        for (int l = 0; l < n; l++)
+                        {
+                            sum = sum + 1;
+                        }
+                    }
+                }
+            }
+        }
+
+        private static void lgngrowth(BigInteger n)
+        {
+            while (n != 1)
+            {
+                n = n / 2;
+            }
+        }
+
+        private static void nFactorial(ulong n)
+        {
+            ulong output = n;
+            for(ulong i = n - 1; i >= 1; i--)
+            {
+                output *= i;
+            }
+            Console.WriteLine(output);
+        }
+    }
+}
--- Project 1/Program.cs
+++ Project 1/Program.cs
@@ -19,6 +19,12 @@ namespace Project_1
 {
     class Program
     {
+
+        /// <summary>
+        /// Main - The method that drives the program.
+        /// </summary>
+        /// <param name="string[] args"></param>
+        /// 
         static void Main(string[] args)
         {
             List<Point> points = new List<Point>();             //a list containing point objects
@@ -26,11 +32,11 @@ namespace Project_1
 
             for (int i = Convert.ToInt32(Console.ReadLine()); i > 0; i--)
             {
-                String instring = Console.ReadLine();
+                String instring = Console.ReadLine();           //string that contains what the user typed in
 
-                string[] values = instring.Split(' ');
+                string[] values = instring.Split(' ');          //array string to split into points format
 
-                Point inPoint = new Point(Convert.ToInt32(values[0]), Convert.ToInt32(values[1]));
+                Point inPoint = new Point(Convert.ToInt32(values[0]), Convert.ToInt32(values[1]));  //create a point object that contains the first and second value. 
 
                 points.Add(inPoint);
             }
