5da9573e86697618fda7e66610788cdc37f40f35
Ryan <ryanthecat17@gmail.com>
2020-03-24 02:41:49 -0400

    proj3

---
+++ Project 3/frm_Main.Designer.cs
@@ -0,0 +1,169 @@
+﻿namespace Project_3
+{
+    partial class frm_Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_Upload = new System.Windows.Forms.Button();
+            this.btn_Popular = new System.Windows.Forms.Button();
+            this.btn_Methylation = new System.Windows.Forms.Button();
+            this.btn_Detox = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.outputBox = new System.Windows.Forms.ListBox();
+            this.label_file = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btn_Upload
+            // 
+            this.btn_Upload.Location = new System.Drawing.Point(12, 12);
+            this.btn_Upload.Name = "btn_Upload";
+            this.btn_Upload.Size = new System.Drawing.Size(103, 23);
+            this.btn_Upload.TabIndex = 0;
+            this.btn_Upload.Text = "Upload File";
+            this.btn_Upload.UseVisualStyleBackColor = true;
+            this.btn_Upload.Click += new System.EventHandler(this.btn_Upload_Click);
+            // 
+            // btn_Popular
+            // 
+            this.btn_Popular.Location = new System.Drawing.Point(121, 14);
+            this.btn_Popular.Name = "btn_Popular";
+            this.btn_Popular.Size = new System.Drawing.Size(123, 37);
+            this.btn_Popular.TabIndex = 1;
+            this.btn_Popular.Text = "Result from Popular Snpedia Items";
+            this.btn_Popular.UseVisualStyleBackColor = true;
+            this.btn_Popular.Click += new System.EventHandler(this.btn_Popular_Click);
+            // 
+            // btn_Methylation
+            // 
+            this.btn_Methylation.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btn_Methylation.Location = new System.Drawing.Point(486, 16);
+            this.btn_Methylation.Name = "btn_Methylation";
+            this.btn_Methylation.Size = new System.Drawing.Size(123, 35);
+            this.btn_Methylation.TabIndex = 2;
+            this.btn_Methylation.Text = "Generate Methylation Analysis";
+            this.btn_Methylation.UseVisualStyleBackColor = true;
+            this.btn_Methylation.Click += new System.EventHandler(this.btn_Methylation_Click);
+            // 
+            // btn_Detox
+            // 
+            this.btn_Detox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btn_Detox.Location = new System.Drawing.Point(250, 14);
+            this.btn_Detox.Name = "btn_Detox";
+            this.btn_Detox.Size = new System.Drawing.Size(230, 37);
+            this.btn_Detox.TabIndex = 3;
+            this.btn_Detox.Text = "Generate Detox Profile";
+            this.btn_Detox.UseVisualStyleBackColor = true;
+            this.btn_Detox.Click += new System.EventHandler(this.btn_Detox_Click);
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(615, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(109, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Algorithms - Project 3 ";
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(615, 25);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(66, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Ryan Shupe";
+            // 
+            // label3
+            // 
+            this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(615, 38);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(161, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "East Tennessee State University";
+            // 
+            // outputBox
+            // 
+            this.outputBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.outputBox.FormattingEnabled = true;
+            this.outputBox.Location = new System.Drawing.Point(12, 58);
+            this.outputBox.Name = "outputBox";
+            this.outputBox.Size = new System.Drawing.Size(764, 277);
+            this.outputBox.TabIndex = 7;
+            // 
+            // label_file
+            // 
+            this.label_file.AutoSize = true;
+            this.label_file.Location = new System.Drawing.Point(12, 38);
+            this.label_file.Name = "label_file";
+            this.label_file.Size = new System.Drawing.Size(75, 13);
+            this.label_file.TabIndex = 8;
+            this.label_file.Text = "No file loaded!";
+            // 
+            // frm_Main
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.ClientSize = new System.Drawing.Size(791, 347);
+            this.Controls.Add(this.label_file);
+            this.Controls.Add(this.outputBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btn_Detox);
+            this.Controls.Add(this.btn_Methylation);
+            this.Controls.Add(this.btn_Popular);
+            this.Controls.Add(this.btn_Upload);
+            this.MinimumSize = new System.Drawing.Size(700, 386);
+            this.Name = "frm_Main";
+            this.Text = "Algorithms - Project 3";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_Upload;
+        private System.Windows.Forms.Button btn_Popular;
+        private System.Windows.Forms.Button btn_Methylation;
+        private System.Windows.Forms.Button btn_Detox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ListBox outputBox;
+        private System.Windows.Forms.Label label_file;
+    }
+}
+
---
+++ Project 3/frm_Main.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Windows.Forms;
+
+namespace Project_3
+{
+    public partial class frm_Main : Form
+    {
+        private List<String[]> entrys = new List<String[]>(); //variable to hold the rsid's that is found in the file
+        private static bool fileIn = false;
+        public frm_Main()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_Upload_Click(object sender, EventArgs e)
+        {
+            StreamReader rdr = null;
+            OpenFileDialog OpenDlg = new OpenFileDialog();
+
+            OpenDlg.Filter = "text files|*.txt;*.text";
+            OpenDlg.InitialDirectory = Application.StartupPath;
+            OpenDlg.Title = "Select a text file to insert into the list";
+
+            if (DialogResult.Cancel != OpenDlg.ShowDialog())
+            {
+                try
+                {
+                    rdr = new StreamReader(OpenDlg.FileName);
+
+                    for (int i = 0; i < 17; i++)
+                    {
+                        rdr.ReadLine();
+                    }
+
+                    while (!rdr.EndOfStream)
+                    {
+                        String sep = "\t";
+                        String[] ent = new String[5];
+
+                        ent = rdr.ReadLine().Split(sep.ToCharArray());
+                        entrys.Add(ent);
+                    }
+                }
+                finally
+                {
+                    if (rdr != null)
+                    {
+                        rdr.Close();
+                        fileIn = true;
+                        label_file.Text = "File is Loaded!";
+                    }
+                }
+
+                string fileName = OpenDlg.FileName;
+            }
+        }
+
+        private void btn_Popular_Click(object sender, EventArgs e)
+        {
+            if (fileIn)
+            {
+                outputBox.Items.Clear();
+
+                for(int i = 0; i < entrys.Count; i++)
+                {
+                    String output = "";
+                    String[] currentEntry = new String[5];
+
+                    currentEntry = entrys[i];
+
+                    //this is where all of the popular items are kept (it works its just really slow)
+                    if(currentEntry[0] == "rs53576")
+                    {
+                        output += "RS53576\t\t";
+                        output += "Description: influences social behavior and personality\t";
+
+                        if (((currentEntry[3] == "A") && (currentEntry[4] == "A")) || ((currentEntry[3] == "A") && (currentEntry[4] == "G")))
+                        {
+                            output += "Magnitude: 2.8\t";
+                            output += "Lack of empathy?";
+                        }
+                        else if (((currentEntry[3] == "G") && (currentEntry[4] == "G")))
+                        {
+                            output += "Magnitude: 2.5\t";
+                            output += "Optimistic and empathetic; handle stress well";
+                        }
+                    }
+
+                    if (currentEntry[0] == "rs1815739")
+                    {
+                        output += "RS1815739\t\t";
+                        output += "Description: muscle performance\t";
+
+                        if ((currentEntry[3] == "C") && (currentEntry[4] == "C")) 
+                        {
+                            output += "Magnitude: 2.2\t";
+                            output += "Better performing muscles. Likely sprinter.";
+                        }
+                        else if (((currentEntry[3] == "T") && (currentEntry[4] == "T")))
+                        {
+                            output += "Magnitude: 2.2\t";
+                            output += "Impaired muscle performance. Likely endurance athlete.";
+                        }
+                        else if (((currentEntry[3] == "C") && (currentEntry[4] == "T")))
+                        {
+                            output += "Magnitude: 2.1\t";
+                            output += "Mix of muscle types. Likely sprinter.";
+                        }
+                    }
+
+                    if (currentEntry[0] == "rs429358" || currentEntry[0] == "rs7412")
+                    {
+                        output += "RS429358 or RS7412\t\t";
+                        output += "Description: raise risk of Alzheimer's\t";
+
+                        if ((currentEntry[3] == "C") && (currentEntry[4] == "C"))
+                        {
+                            output += "Magnitude: 1.2\t";
+                            output += "one of 2 snps relevant to classifying APOE genotype";
+                        }
+                        else if (((currentEntry[3] == "T") && (currentEntry[4] == "T")))
+                        {
+                            output += "Magnitude: null\t";
+                            output += ">3x increased risk for Alzheimer's; 1.4x increased risk for heart disease";
+                        }
+                        else if (((currentEntry[3] == "C") && (currentEntry[4] == "T")))
+                        {
+                            output += "Magnitude: 0\t";
+                            output += "Common";
+                        }
+                    }
+
+                    if (currentEntry[0] == "rs6152")
+                    {
+                        output += "RS6152\t\t";
+                        output += "Description: baldness\t";
+
+                        if ((currentEntry[3] == "A") && (currentEntry[4] == "A"))
+                        {
+                            output += "Magnitude: 4\t";
+                            output += "won't go bald";
+                        }
+                        else if (((currentEntry[3] == "A") && (currentEntry[4] == "G")))
+                        {
+                            output += "Magnitude: 2\t";
+                            output += "	Increased risk of baldness";
+                        }
+                        else if (((currentEntry[3] == "G") && (currentEntry[4] == "G")))
+                        {
+                            output += "Magnitude: 0.5\t";
+                            output += "able to go bald";
+                        }
+                        else if ((currentEntry[3] == "A"))
+                        {
+                            output += "Magnitude: 3\t";
+                            output += "";
+                        }
+                    }
+
+
+                    output += "\n\n";
+                    outputBox.Items.Add(output);
+                }
+            }
+            else
+            {
+                outputBox.Items.Clear();
+                outputBox.Items.Add("No file loaded!");
+            }
+        }
+
+        private void btn_Detox_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void btn_Methylation_Click(object sender, EventArgs e)
+        {
+        }
+
+
+    }
+}
