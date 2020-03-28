097ab3083fe767776c8376965f443d46dbbf27a5
RSWaffle <ryanthecat17@gmail.com>
2020-03-28 15:52:16 -0400

    heap insert

---
+++ Heap Insert/Heap Insert/MaxHeap.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Heap_Insert
+{
+    internal class MaxHeap
+    {
+        private int max_size;           //max size of heap, this is included just to explain that size is not maximum size, but is current heap size.
+        private int size;               //size of current heap
+        private string[] h;             //array of strings that the heap will use
+
+        public MaxHeap()
+        {
+        }
+
+        public MaxHeap(int inSize)
+        {
+            size = 0;
+            max_size = inSize;
+            h = new string[max_size + 1];
+        }
+
+        public void Insert(String item)
+        {
+            h[size + 1] = item;
+            size++;
+
+            int fixsize = size;
+            do
+            {
+                if (size == 1)
+                {
+                    break;
+                }
+                int currentNode = fixsize;
+                int parentNode = fixsize / 2;
+                fixsize /= 2;
+                string temp;
+                if (String.CompareOrdinal(h[parentNode], h[currentNode]) < 0)
+                {
+                    temp = h[parentNode];
+                    h[parentNode] = h[currentNode];
+                    h[currentNode] = temp;
+                }
+                else
+                {
+                    break;
+                }
+            } while (fixsize != 1);
+        }
+
+        public string print()
+        {
+            string output = "";
+            for (int i = 0; i < size; i++)
+            {
+                output += h[i + 1] + " ";
+            }
+            return output;
+        }
+    }
+}
---
+++ Heap Insert/Heap Insert/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Heap_Insert
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the number of Strings you wish to store in the heap: ");
+            int strings = Convert.ToInt32(Console.ReadLine());
+            MaxHeap heap = new MaxHeap(strings);
+
+            for (int i = 0; i < strings; i++)
+            {
+                heap.Insert(Console.ReadLine());
+                Console.WriteLine(heap.print());
+            }
+
+            //Console.WriteLine(heap.print());
+            Console.ReadLine();
+        }
+    }
+}
